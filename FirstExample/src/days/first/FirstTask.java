78f42927c386390fe9e906516ea306c95a009b96
raincoatt9 <raincoatt9@gmail.com>
2020-04-16 19:07:55 +0300

    pirmas commitas

---
+++ FirstExample/src/days/first/FirstTask.java
@@ -0,0 +1,55 @@
+package days.first;
+
+import java.util.Scanner;
+
+
+/**
+
+Justas
+
+*/
+public class FirstTask
+{
+    public static void main(String[] args)
+    {
+
+       Scanner sc = new Scanner(System.in);
+       String word;
+       do{
+           System.out.println("iveskite zodi arba pabaiga darbo baigimui");
+           word = sc.nextLine();
+
+           checkOrWordIsEven(word);
+           System.out.println("A raidziu zodyje yra: " + countLetters(word,'a'));
+
+
+       }while(!"pabaiga".equalsIgnoreCase(word));
+
+    }
+    private static void checkOrWordIsEven(String word)
+    {
+        if(word.length() % 2 == 0)
+        {
+            System.out.println(String.format("ivestas zodis %s yra lyginis", word));
+
+        }
+        else
+        {
+            System.out.println(String.format("ivestas zodis %s yra nelyginis", word));
+        }
+    }
+
+    private static int countLetters(String word, char letter)
+    {
+        int count = 0;
+        for (int i = 0; i < word.length(); i++)
+        {
+            {
+                if (letter == word.charAt(i))
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ FirstExample/src/days/first/SecondTask.java
@@ -0,0 +1,41 @@
+package days.first;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Justas
+ */
+
+public class SecondTask
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Iveskite norima polindroma, o mes patikrinsim");
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.nextLine().replaceAll(" ", "");
+        if (isPalindrome(word))
+        {
+            System.out.println("Palindrome");
+        }
+        else
+        {
+            System.out.println("word is not a Palindrome");
+        }
+    }
+
+    private static boolean isPalindrome(String word)
+    {
+        for (int i = 0; i< word.length(); i++)
+        {
+            if (word.charAt(i) != word.charAt(word.length() -1 -i ))
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
